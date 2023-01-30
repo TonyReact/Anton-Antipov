18be7fa946dbeb357f44e8f40cd5b2e326bda881
Tony <99402558+TonyReact@users.noreply.github.com>
2023-01-31 00:55:27 +0200

    Add files via upload

---
+++ sirma/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import Homepage from "./components/Homepage";
+import "./styles/global.scss";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <div className="loader-container">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        <div>
+          <Homepage />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default App;
